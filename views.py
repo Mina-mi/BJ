4ce6c973f68761bde23eb2c74d59262c6a814551
Mina-mi <49612620+Mina-mi@users.noreply.github.com>
2019-04-14 22:19:42 +0900

    Add files via upload

--- views.py
+++ views.py
@@ -29,6 +29,7 @@ def game(request):
             'money' : 100,
         }
         dictionary.update(csrf(request))
+        return render(request, 'bjgame2.html', dictionary)
 
         return render(request, 'bjgame.html', dictionary)
     elif request.method == 'POST':
@@ -40,7 +41,22 @@ def game(request):
         dealer_hands = r.get_redis(token, 'dealer_hands')
 
         if (r.get_redis(token, 'game_now') == False):
-
+                try:
+                if money < int(request.POST['bet']):
+                    raise ValueError('too large bet')
+                except ValueError:
+                    msg = '正しくベットしてください'
+                    dictionary = {
+                        'msg' : msg
+                        'dealer_cards' : [],
+                        'dealer_point' : 0,
+                        'player_cards' : [],
+                        'player_point' : 0,
+                        'able_bet' : True,
+                        'money' : money,
+                    }
+                    dictionary.update(csrf(request))
+                    return render(request, 'bjgame2.html', dictionary)
             r.set_redis(token, 'game_now', True)
             money -= int(request.POST['bet'])
             r.set_redis(token, 'money', money)
@@ -61,9 +77,9 @@ def game(request):
 
             dictionary = {
                 'msg' : '選択してください.',
-                'dealer_cards' : dealer_hands,
+                'dealer_cards' : conv_image_path(dealer_hands),
                 'dealer_point' : dealer_point,
-                'player_cards' : player_hands,
+                'player_cards' : conv_image_path(player_hands),
                 'player_point' : player_point,
                 'able_bet' : False,
                 'able_double' : True,
@@ -71,7 +87,8 @@ def game(request):
                 'bet' : int(request.POST['bet']),
             }
             dictionary.update(csrf(request))
-            return render(request, 'bjgame.html', dictionary)
+            return render(request, 'bjgame2.html', dictionary)
+
         else:
             op = request.POST['operation']
             bet = r.get_redis(token, 'bet')
@@ -96,15 +113,15 @@ def game(request):
                 player_point = bj.get_point(player_hands)
                 msg, money = bj.win_lose(dealer_hands, player_hands, bet, money)
                 if money <= 0:
-                    return HttpResponse('GameOver')
+                    return render(request, 'gameover.html')
                 r.set_redis(token, 'money', money)
                 msg += 'ベットしてください.'
 
                 dictionary = {
                     'msg' : msg,
-                    'dealer_cards' : dealer_hands,
+                    'dealer_cards' : conv_image_path(dealer_hands),
                     'dealer_point' : bj.get_point(dealer_hands),
-                    'player_cards' : player_hands,
+                    'player_cards' : conv_image_path(player_hands),
                     'player_point' : bj.get_point(player_hands),
                     'able_bet' : True,
                     'money' : money,
@@ -115,18 +132,22 @@ def game(request):
                 r.set_redis(token, 'deck', deck)
                 r.set_redis(token, 'game_now', False)
 
-                return render(request, 'bjgame.html', dictionary)
+                return render(request, 'bjgame2.html', dictionary)
+
             else:
                 r.set_redis(token, 'deck', deck)
 
                 dictionary = {
-                    'dealer_cards' : dealer_hands,
+                    'dealer_cards' : conv_image_path(dealer_hands),
                     'dealer_point' : bj.get_point(dealer_hands),
-                    'player_cards' : player_hands,
+                    'player_cards' : conv_image_path(player_hands),
                     'player_point' : player_point,
                     'able_bet' : False,
                     'money' : money,
                     'bet' : bet,
                 }
                 dictionary.update(csrf(request))
-                return render(request, 'bjgame.html', dictionary)
+                return render(request, 'bjgame2.html', dictionary)
+
+def howto(request):
+    return render(request, 'howto.html')
